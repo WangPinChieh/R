dcb2b6d4f85116f3e68926e04c766be6ef600d44
WangPinChieh <pp8101313@hotmail.com>
2015-07-23 15:37:59 +0800

    update

--- german.R
+++ german.R
@@ -1,29 +1,25 @@
 german<-read.csv(file="D:/R/german.csv", sep=",", header=TRUE)
 
-head(german)
 
-newData <- na.omit(german)
-
-y <- factor(newData$class)
-x1 <- newData$duration
-x2 <- newData$purpose
-x3 <- newData$savings_status
-x4 <- newData$age
+y <- factor(german$class)
+x1 <- german$duration
+x2 <- german$purpose
+x3 <- german$savings_status
+x4 <- german$age
 
 
 lm.model <- glm(y ~ x2, data=german, family=binomial("logit"))
 
-test <-newData[c("class", "purpose")]
+test <-german[c("class", "purpose")]
 test$class <- as.factor(test$class)
 test$purpose <- as.factor(test$purpose)
 
-within(test, {
-  purpose[purpose=="X"] <- NA
-  purpose[purpose==""] <- NA
-})
-test<- na.omit(test)
 
 summary(test)
+
+
+
+
 
 # apply practices
 m <- matrix(c(1:3), nrow=1, ncol=3)
