b333ce85d4caf38aa23f5bc2e6754b0469c0588c
WangPinChieh <pp8101313@hotmail.com>
2015-07-23 11:42:21 +0800

    update

--- patient.R
+++ patient.R
@@ -2,11 +2,19 @@ patientID <- c(1,2,3,4)
 age <- c(25,34,28,52)
 diabetes <- c("Type1", "Type2", "Type1", "Type2")
 diabetes <- factor(diabetes)
-status <- c("Poor", "Improved", "Excellent", "Poor")
-status <- factor(status, ordered = TRUE, levels = c("Poor","Improved","Excellent"))
+status <- c("Poor", "Poor", "Excellent", "Poor")
+status <- factor(status, ordered = TRUE, levels = c("Poor","Excellent"))
 patientdata <- data.frame(patientID, age, diabetes, status, row.names = patientID)
 
-#Reading csv file through url
-urlfile <- "http://ichart.yahoo.com/table.csv?s=2002.TW"
-Stock2002TW <- read.csv(url(urlfile))
-#End reading csv file through url
+y <- patientdata$status
+x1 <- patientdata$age
+x2 <- patientdata$diabetes
+
+ml.logit <- glm(y ~ x1 + x2, family = binomial(link = "logit") ,data = patientdata)
+summary(ml.logit)
+
+newdata <- data.frame(x1 = c(30), x2 = factor(c("Type2")))
+
+result <- predict(ml.logit, newdata)
+
+result
