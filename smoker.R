73b0137fb7338d7383150771e470351c4f3a39ad
WangPinChieh <pp8101313@hotmail.com>
2015-07-27 14:06:22 +0800

    update

---
+++ smoker.R
@@ -0,0 +1,12 @@
+smokerData <- read.csv(file="smoker.csv", sep=",", header = T)
+
+smoke <- table(smokerData$Smoke, smokerData$SES)
+
+barplot(smoke, legend=T, beside=T, main='Smoking status by SES')
+margin.table(smoke) # total number of data
+margin.table(smoke,1) # total number of data summed by row
+margin.table(smoke,2) #total number of data summed by column
+
+smoke/margin.table(smoke)
+# is equivalent to
+prop.table(smoke)
