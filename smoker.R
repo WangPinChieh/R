ab63ddf5b7544e914158d9fcc125c06278693aaa
WangPinChieh <pp8101313@hotmail.com>
2015-07-28 10:31:43 +0800

    update

--- smoker.R
+++ smoker.R
@@ -10,3 +10,9 @@ margin.table(smoke,2) #total number of data summed by column
 smoke/margin.table(smoke)
 # is equivalent to
 prop.table(smoke)
+
+
+
+expected <- as.array(margin.table(smoke,1)) %*% t(as.array(margin.table(smoke,2)))/margin.table(smoke)
+
+chi <- sum((expected - as.array(smoke))^2/expected)
